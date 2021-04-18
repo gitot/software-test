81630a3b720891e28f568090ddf9ae138b1178de
wangguiyong <1571337712@qq.com>
2021-04-18 13:54:32 -0700

    testng simple demo

---
+++ testng-demo/src/main/java/Stack.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack {
+
+    List<Integer> data = new ArrayList<>();
+
+    public Integer pop() {
+        if (size() <= 0) throw new IllegalStateException("have no element");
+        return data.get(size() - 1);
+    }
+
+    public void push(int i) {
+        data.add(i);
+    }
+
+    public int size() {
+        return data.size();
+    }
+}
---
+++ testng-demo/src/test/java/StackTest.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StackTest {
+
+    private Stack stack;
+
+    @BeforeMethod
+    public void setUp() {
+        stack = new Stack();
+    }
+
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void testPop_whenNoElements_shouldThrowException() {
+        assertThat(stack.pop());
+    }
+
+    @Test
+    public void testPush() {
+        stack.push(1);
+        assertThat(stack.size()).isEqualTo(1);
+    }
+}
